401a1bab092d1c115878f57afb7316b0a17dc431
parthpilkhan <parthpilkhann@gmail.com>
2024-10-23 22:33:59 +0530

    added code and tests

--- src/helpers/client.test.ts
+++ src/helpers/client.test.ts
@@ -3,3 +3,11 @@ import { add } from "./client";
 test("should return 0 for an empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("should return number when number is provided", () => {
+  expect(add("2")).toBe(2);
+});
+
+test("should return the sum of two numbers", () => {
+  expect(add("1,5")).toBe(6);
+});
--- src/helpers/client.ts
+++ src/helpers/client.ts
@@ -1,27 +1,5 @@
 export function add(numbers: string): number {
-  if (!numbers) return 0;
-
-  const delimiterRegex = /\/\/(.+)\n/;
-  let delimiter = ",";
-  let numberString = numbers;
-
-  const customDelimiterMatch = numbers.match(delimiterRegex);
-  if (customDelimiterMatch) {
-    delimiter = customDelimiterMatch[1];
-    numberString = numbers.split("\n")[1];
-  }
-
-  const sanitizedNumbers = numberString.replace(/\n/g, delimiter);
-  const numberArray = sanitizedNumbers
-    .split(delimiter)
-    .map((num) => parseInt(num, 10));
-
-  const negativeNumbers = numberArray.filter((num) => num < 0);
-  if (negativeNumbers.length) {
-    throw new Error(
-      `Negative numbers not allowed: ${negativeNumbers.join(", ")}`
-    );
-  }
-
-  return numberArray.reduce((sum, num) => sum + num, 0);
+  if (numbers === "") return 0;
+  const nums = numbers.split(",").map(Number);
+  return nums.reduce((sum, num) => sum + num, 0);
 }
