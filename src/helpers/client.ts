79cdbd493c08509164363740f7f9ae7bd45fdade
parthpilkhan <parthpilkhann@gmail.com>
2024-11-12 17:56:49 +0530

    minor

--- src/helpers/client.ts
+++ src/helpers/client.ts
@@ -7,47 +7,26 @@ export function add(numbers: string): number {
 
   if (numbers.startsWith("//")) {
     const delimiterEnd = numbers.indexOf("\n");
-
     const customDelimiter = numbers.substring(2, delimiterEnd);
-
-    // Simplified to use a single custom delimiter
     delimiter = new RegExp(escapeRegExp(customDelimiter));
-
     numbers = numbers.substring(delimiterEnd + 1);
   }
 
-  const nums = numbers.split(delimiter);
+  const nums = numbers
+    .split(delimiter)
+    .map((num) => Number(num.trim()))
+    .filter((num) => !isNaN(num));
 
-  const validNums = [];
-  const negatives = [];
-
-  for (let i = 0; i < nums.length; i++) {
-    const num = Number(nums[i].trim());
-
-    if (!isNaN(num)) {
-      validNums.push(num);
-
-      if (num < 0) {
-        negatives.push(num);
-      }
-    }
-  }
+  const negatives = nums.filter((num) => num < 0);
 
   if (negatives.length > 0) {
     throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
   }
 
-  let sum = 0;
-
-  for (let i = 0; i < validNums.length; i++) {
-    sum += validNums[i];
-  }
-
-  return sum;
+  return nums.reduce((sum, num) => sum + num, 0);
 }
 
 // This ensures the custom delimiters don't break the regular expressions
 function escapeRegExp(string: string): string {
-  // Replace special characters with a backslash followed by the character
   return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 }
